0d922b715a7cd03444df7f82ebee7add043f72af
ltsai323 <johnson20050@gmail.com>
2023-12-21 17:30:49 +0800

    add tester website with button. This button is able to control the power supply

---
+++ flaskUI/app.button_send_cmd_via_socket.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index_button_send_cmd_via_socket.html')
+
+@app.route('/send_message')
+def send_message():
+    message = "1"
+    send_socket_message(message)
+    return "Message sent: {}".format(message)
+@app.route('/off_PS')
+def status_off_powersupply():
+    message = "0"
+    send_socket_message(message)
+    return "Message sent: {}".format(message)
+
+def send_socket_message(message):
+    target_address = '127.0.0.1'
+    target_port = 2234
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((target_address, target_port))
+        s.sendall(message.encode())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
