2b308928d31ea4af083f8ef738c218efac7b134d
ltsai <ltsai@localhost.localdomain>
2024-01-03 08:39:32 -0500

    add automator

--- PyModule_USB2RS232_PowerSupply/run.sh
+++ PyModule_USB2RS232_PowerSupply/run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
-python3 server.py
-#sudo docker start test-pymodule
+#sudo docker build -t  pymodule-usbrs232-powersupply .
+sudo docker run --device=/dev/ttyUSB0 --name pymodule-usbrs232-powersupply -p 2235:2234 pymodule-usbrs232-powersupply
+#sudo docker run --name pymodule-usbrs232-powersupply -p 2235:2234 pymodule-usbrs232-powersupply
+#sudo docker stop pymodule-usbrs232-powersupply
+##sudo docker start pymodule-usbrs232-powersupply
+#sudo docker rm pymodule-usbrs232-powersupply
