d8896ad51ddc4b40c62fe41a485869fa0afc12fb
ltsai <ltsai@localhost.localdomain>
2024-01-29 14:07:14 +0800

    still dev

--- CommandPost/client.py
+++ CommandPost/client.py
@@ -1,28 +1,18 @@
 import socket
 import threading
-from tools.MesgHub import MesgEncoder_JSON
+from tools.MesgHub import MesgEncoder_JSON,MesgDecoder_JSON
 import tools.SocketCommands as SOC_CMD
+import time
 
 
 def receive_feedback(client_socket):
-    while True:
-        feedback = client_socket.recv(1024).decode('utf-8')
+    while client_socket:
+        #feedback = client_socket.recv(1024).decode('utf-8')
+        feedback = MesgDecoder_JSON(client_socket.recv(1024))
         if not feedback:
             break
         print(f"Received feedback from server: {feedback}")
-
-def main():
-    # Configure the client to connect to the server
-    server_ip = '0.0.0.0'  # Replace with the actual server IP
-    server_port = 3000
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, server_port))
-
-    # Start a thread to receive feedback from the server
-    receive_thread = threading.Thread(target=receive_feedback, args=(client_socket,))
-    receive_thread.start()
-
+def sendcmd_feedback(client_socket):
     idx = 0
     while True:
         # Get user input for the command
@@ -32,9 +22,28 @@ def main():
         # Send the command to the server
         client_socket.send(mesg)
         if command == SOC_CMD.CLOSE or command == SOC_CMD.SHUTDOWN:
+            client_socket.close()
             break
+        time.sleep(0.5)
 
-    client_socket.close()
+def main():
+    # Configure the client to connect to the server
+    server_ip = '0.0.0.0'  # Replace with the actual server IP
+    server_port = 2000
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+
+    # Start a thread to receive feedback from the server
+    receive_thread = threading.Thread(target=receive_feedback, args=(client_socket,))
+    sendcmd_thread = threading.Thread(target=sendcmd_feedback, args=(client_socket,))
+    receive_thread.start()
+    sendcmd_thread.start()
+    receive_thread.join()
+    sendcmd_thread.join()
+
+
+    #client_socket.close()
 
 if __name__ == "__main__":
     main()
--- CommandPost/structure.py
+++ CommandPost/structure.py
@@ -4,11 +4,11 @@ import time
 import tools.StatusDefinition as STAT_DEF
 import tools.SocketCommands as SOC_CMD
 from tools.LogTool import LOG
-from tools.MesgHub import MesgDecoder_JSON
+from tools.MesgHub import MesgDecoder_JSON,MesgEncoder_JSON
 
 from typing import Callable
 BIND_ADDR = '0.0.0.0'
-BIND_PORT = 3000
+BIND_PORT = 2000
 
 STAT_REPORT_PERIOD = 3
 
@@ -29,7 +29,6 @@ class CommandPost:
         recFUNC is a function used to handle received message from client
         The client should send a list of dictionary using json package.
         And the function is able to receive that and decide how to response
-
         statusHUB records the status of each individual modules
         '''
         self.receive_func = recFUNC
@@ -37,6 +36,7 @@ class CommandPost:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((BIND_ADDR, BIND_PORT))
         self.server_socket.listen(5)
+        self.server_socket.settimeout(2.0)
 
         self.orig_address = None
         self.self_mesg = message_center('self')
@@ -52,12 +52,23 @@ class CommandPost:
 
             client_socket, address = self.server_socket.accept()
             LOG('new session', 'run', f'connected address {address}')
+
             if self.orig_address == None:
+                # accept first connection
                 self.orig_address = address
             elif self.orig_address != address:
-                LOG('Refused connection', 'run', f'new connection from {address} refused')
+                ## basically reject second connection. But if the second connection send "-FORCEDCLOSECURRENTCONNECTION-"
+                ## command, disconnect current session
+                # asdf no idea how to do
+                #cmds = MesgDecoder_JSON(client_socket.recv(800)) # very small window for receive message
+                #LOG('Refused connection', 'run', f'new connection from {address} refused')
+                #for cmd in cmds:
+                #    if cmd['cmd'] == SOC_CMD.FORCED_CLOSE_CURRENT_CONNECTION:
+                #        client_socket.close()
+                #        self.orig_address = None
+                #        LOG('Clear current connection', 'cmd_receive_feedback', f'clear current connection')
+                #        break
                 continue
-            # Accept a client connection
             LOG('Session accept', 'run', f"Accepted connection from {address}")
 
             # Start threads for sending and receiving
@@ -71,6 +82,21 @@ class CommandPost:
             #send_status_thread.join()
             cmd_receive_thread.join()
             LOG('Session ended', 'run', f'connection from {address} ended')
+    def run_(self):
+        LOG('run start', 'run', 'init')
+
+        # threading
+        my_threads = []
+        my_threads.append(threading.Thread(target=self.connection_handler))
+        my_threads.append(threading.Thread(target=self.main_recv))
+
+        for t in my_threads: t.start()
+        for t in my_threads: t.join()
+
+        #connection_handler()
+        #main_recv()
+        #periodically_report()
+
     def SetMesg(self,name,status,mesg):
         if not hasattr(self.mesgs, name):
             raise IOError(f'input message {name} not found')
@@ -86,15 +112,112 @@ class CommandPost:
 
 
 
+            # Accept a client connection
+    def connection_handler(self):
+        while True:
+            if self.server_socket == None:
+                LOG('Whole program is closed...', 'connection_handler', 'No server_socket object available.')
+                return
 
+            try:
+                client_socket, address = self.server_socket.accept()
+                LOG('new session', 'connection_handler', f'connected address {address}')
+
+                if self.orig_address == None:
+                    # accept first connection
+                    self.client_socket = client_socket
+                    self.orig_address = address
+                    #self.client_socket.settimeout(2)
+                    LOG('Session accept', 'run', f"Accepted connection from {address}")
+                if self.orig_address != address:
+                    ## basically reject second connection. But if the second connection send "-FORCEDCLOSECURRENTCONNECTION-"
+                    ## command, disconnect current session
+                    cmds = MesgDecoder_JSON(client_socket.recv(800)) # very small window for receive message
+                    for cmd in cmds:
+                        if cmd['cmd'] == SOC_CMD.FORCED_CLOSE_CURRENT_CONNECTION:
+                            client_socket.close()
+                            self.orig_address = None
+                            LOG('Clear current connection', 'cmd_receive_feedback', f'clear current connection')
+                            break
+                    LOG('Refused connection', 'run', f'new connection from {address} refused')
+            except:
+                return
+            #except socket.timeout:
+            #    print('connection_handler timeout!!!!')
+            #    time.sleep(2)
+
+
+    def main_recv(self):
+        while True:
+            if self.server_socket == None:
+                LOG('Whole program is closed...', 'main_recv', 'No server_socket object available.')
+                return
+            if self.orig_address == None:
+                time.sleep(1)
+                print('Waiting connection', 'main_recv', 'waiting for client connection...')
+                continue
+
+            # start communication
+            #LOG('new session', 'run', f'connected address {address}')
+
+            socket_connection = self.client_socket
+            LOG('Connection Accepted', 'main_recv', 'from address "{self.orig_address}"')
+            while True:
+                try:
+                    cmds = MesgDecoder_JSON(socket_connection.recv(1024))
+
+                    print(f'Received command from client : {cmds}')
+                    terminateThisSession = False
+                    for cmd in cmds:
+                        if not isinstance(cmd,dict):
+                            LOG('Unknown input', 'CommandPost', f'Invalid command "{cmd}" received. Close this session.')
+                            socket_connection.close()
+                            self.orig_address = None
+                            terminateThisSession = True
+                            break
+                        if cmd['cmd'] == SOC_CMD.CLOSE:
+                            socket_connection.close()
+                            LOG('Client Disconnected', 'CommandPost', f'Disconnect from {self.orig_address}, waiting for new connection...')
+                            self.orig_address = None
+                            terminateThisSession = True
+                            break
+                        if cmd['cmd'] == SOC_CMD.SHUTDOWN:
+                            socket_connection.close()
+                            self.server_socket.close()
+                            self.server_socket = None
+                            LOG('Shut down', 'main_recv', 'Received the request to shutdown the whole program')
+                            return
+
+                        LOG('Before modification', 'cmd_receive_feedback', f'current test mesg is : {self.mesgs["test"].message}')
+                        self.receive_func(cmd,self.mesgs) # asdf need to use thread for parallel execution
+                        LOG('After  modification', 'cmd_receive_feedback', f'current test mesg is : {self.mesgs["test"].message}')
+
+                    if terminateThisSession: break
+
+                except self.client_socket.timeout:
+                    print('main_recv timeout!!!!')
+                    pass
+                except OSError as e: # if the connection is closed
+                    print('section closed')
+                    print(f'original addresss : {self.orig_address}. Client {self.client_socket}')
+
+                    LOG('ERROR', 'main_recv', e)
+                    time.sleep(2)
+                    socket_connection.close()
+                    self.orig_address = None
+                    continue
     def cmd_receive_feedback(self,socket_connection):
         while True:
             try:
-                #cmd = socket_connection.recv(1024).decode('utf-8')
                 cmds = MesgDecoder_JSON(socket_connection.recv(1024))
+
                 print(f'Received command from client : {cmds}')
                 for cmd in cmds:
-                    print(cmd)
+                    if not isinstance(cmd,dict):
+                        LOG('Unknown input', 'CommandPost', f'Invalid command "{cmd}" received. Close this session.')
+                        socket_connection.close()
+                        self.orig_address = None
+                        return
                     if cmd['cmd'] == SOC_CMD.CLOSE:
                         socket_connection.close()
                         LOG('Client Disconnected', 'CommandPost', f'Disconnect from {self.orig_address}, waiting for new connection...')
@@ -106,15 +229,17 @@ class CommandPost:
                         self.server_socket = None
                         LOG('Shut down', 'CommandPost', 'Received the request to shutdown the whole program')
                         return
+                    LOG('Before modification', 'cmd_receive_feedback', f'current test mesg is : {self.mesgs["test"].message}')
                     self.receive_func(cmd,self.mesgs) # asdf need to use thread for parallel execution
+                    LOG('After  modification', 'cmd_receive_feedback', f'current test mesg is : {self.mesgs["test"].message}')
 
             except OSError as e: # if the connection is closed
                 print('section closed')
+                socket_connection.close()
                 return
 
 
     def send_status_commands(self,socket_connection):
-        idx = 0
         try:
             while True:
                 time.sleep(STAT_REPORT_PERIOD)
@@ -125,9 +250,7 @@ class CommandPost:
                     report.append( { 'name':name, 'stat':stat.status, 'mesg':stat.message} )
 
                 if len(report) == 0: continue
-                # Send the status to the server
-                status = '0' # asdf
-                socket_connection.send(status.encode('utf-8'))
+                socket_connection.sendall(MesgEncoder_JSON(*report))
         except OSError as e: # if the connection is closed
             print('section closed')
             return
@@ -137,12 +260,16 @@ class CommandPost:
 
 if __name__ == "__main__":
     def command_pool(cmd, mesgHUB:dict): # command executed parallelly
-        if cmd['name'] == 'test': print(f'hi test  sample received with command {cmd["cmd"]}')
+        if cmd['name'] == 'test':
+            print(f'hi test  sample received with command {cmd["cmd"]}')
+            mesgHUB['test'].status = 3
+            mesgHUB['test'].message = f'here is modified test message. Receiving the command "{cmd["cmd"]}"'
 
     obj1 = message_center('PowerSupply')
     obj2 = message_center('USB_RS232')
     obj3 = message_center('test')
     mainobj = CommandPost(command_pool, obj1,obj2,obj3)
-    mainobj.run()
+    #mainobj.run()
+    mainobj.run_()
 
 
