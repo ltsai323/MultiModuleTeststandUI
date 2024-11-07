4401d42d3d084de7c4f42c9b6e94aeb8aed77dbf
ltsai <ltsai@hep1.phys.ntu.edu.tw>
2024-11-07 20:00:49 +0800

    sync

--- app_old.py
+++ app_old.py
@@ -3,12 +3,15 @@ from app_global_variables import _VARS_, _LOG_CENTER_, _JOB_STAT_
 import app_bkgrun
 import app_actbtn
 from DebugManager import BUG
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__, static_folder='./static')
-#app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Required for CSRF protection asdf
+# add csrf protect asdf
+app.secret_key = 'your_secret_key'  # Required for CSRF protection. The availability checking required
+csrf = CSRFProtect(app)
 
 
+# def current_status() # new function for sockeio
 
 
 @app.route('/')
@@ -35,21 +38,26 @@ This page shows action buttons configured from `app_actbtn.py`.
 And `app_bkgrun.py` configures the jobs sent to bkg
     '''
 
-    #form_dict = app_actbtn.AllAvailableInputFields()
+    # I should delete this block but later
+    ''' '''
+    form_dict = app_actbtn.AllAvailableInputFields()
 
-    #available_form_dict = {}
-    #for n, v in form_dict.items():
-    #    field_names = [field.type for field in v]
-    #    if len(field_names) > 1:
-    #        BUG(f'all available fields in [{n}]: ', field_names)
-    #        available_form_dict[n] = v
+    available_form_dict = {}
+    for n, v in form_dict.items():
+        field_names = [field.type for field in v]
+        if len(field_names) > 1:
+            BUG(f'all available fields in [{n}]: ', field_names)
+            available_form_dict[n] = v
 
 
 
-    #BUG('get current status : ', app_bkgrun.get_current_status())
-    #available_buttons = app_bkgrun.get_current_status()
-    #the_stat = app_bkgrun.app_b.web_stat
-    #print(available_form_dict)
+    BUG('get current status : ', app_bkgrun.get_current_status())
+    available_buttons = app_bkgrun.get_current_status()
+    the_stat = app_bkgrun.app_b.web_stat
+    ''' '''
+
+
+    # should add fetch() function to index.html that require current status from api/current_status or use "initialize" button
 
     return render_template('index.html')
     #return render_template('index.html', forms = available_form_dict, availableBUTTONs = available_buttons, webSTAT = the_stat)
