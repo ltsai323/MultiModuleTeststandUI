5396b923bad08eeb41365884868cdb2dbfa5f21b
ltsai323 <johnson20050@gmail.com>
2024-11-15 14:47:23 +0800

    let global variable initialized in app.py with BasicConfig

--- app_global_variables.py
+++ app_global_variables.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 import queue
+import WebStatus
 
 class GlobalVars:
     def __init__(self):
         self.cmders = {}
         #self.jobstat = {}
 
+### old variables
 _VARS_ = GlobalVars()
 _LOG_CENTER_ = queue.Queue()
 _JOB_STAT_ = {}
 
+### new variable
+DEBUG = 'False'
+WEB_STAT = 'WebStatus.WebStatus()'
+JOB_QUEUE = 'queue.Queue()'
 
+
+
+
+class BasicConfig:
+    DEBUG = True
+    WEB_STAT = WebStatus.WebStatus()
+    JOB_QUEUE = queue.Queue()
+class TestConfig(BasicConfig):
+    DEBUG = True
+    WEB_STAT = WebStatus.WebStatus()
+    JOB_QUEUE = queue.Queue()
+
+# use app.config.from_object(TestConfig) to initialize the variables
