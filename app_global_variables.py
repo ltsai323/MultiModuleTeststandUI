3e9e1f7628d810d156a13ae102a2d0ef07cc1900
ltsai323 <johnson20050@gmail.com>
2024-11-17 12:46:46 +0800

    add logger

--- app_global_variables.py
+++ app_global_variables.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import queue
 import WebStatus
+import logging
 
 class GlobalVars:
     def __init__(self):
@@ -17,6 +18,9 @@ DEBUG = 'False'
 WEB_STAT = 'WebStatus.WebStatus()'
 JOB_QUEUE = 'queue.Queue()'
 
+# Configure the logging
+
+MESG_LOG = 'logging.getLogger("AppLogger")'
 
 
 
@@ -24,9 +28,23 @@ class BasicConfig:
     DEBUG = True
     WEB_STAT = WebStatus.WebStatus()
     JOB_QUEUE = queue.Queue()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    MESG_LOG = logging.getLogger("AppLogger")
 class TestConfig(BasicConfig):
     DEBUG = True
     WEB_STAT = WebStatus.WebStatus()
     JOB_QUEUE = queue.Queue()
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s:%(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    MESG_LOG = logging.getLogger("AppLogger")
 
 # use app.config.from_object(TestConfig) to initialize the variables
