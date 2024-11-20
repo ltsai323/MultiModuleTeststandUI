f8d7d1b4cfebf53e17ef11e16c1133fed5e808e7
ltsai323 <johnson20050@gmail.com>
2024-11-20 18:50:14 +0800

    add desc

--- ModuleBase.py
+++ ModuleBase.py
@@ -2,6 +2,10 @@
 class ModuleBase:
     '''
     only define the structure.
+
+    Module classes are used to define a whole job.
+    Loaded a lots of job instances and run them in parallel or sequential.
+    Such as we can manage the scheduler to arrange the jobs.
     '''
     def __init__(self):
         pass
