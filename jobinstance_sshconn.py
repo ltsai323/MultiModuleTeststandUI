63a32a940a50477ecdce8467349612b473633376
ltsai323 <johnson20050@gmail.com>
2024-11-20 18:43:46 +0800

    move yaml config setups into this file

--- jobinstance_sshconn.py
+++ jobinstance_sshconn.py
@@ -92,7 +92,7 @@ def test_direct_run():
     ssh_client.close()
     log_stdout.info('end of test_direct_run()')
 
-class JobUnit:
+class JobInstance:
     def __init__(self, hostNAME:str, userNAME:str, privateKEYfile:str, timeOUT:float,
                  stdOUT, stdERR,
                  cmdTEMPLATEs:dict, argCONFIGs:dict):
@@ -108,6 +108,7 @@ class JobUnit:
         self.err = stdERR
         self.config = argCONFIGs
         self.cmd_template = cmdTEMPLATEs
+
     def Initialize(self):
         try:
             self.ssh_client.connect(hostname=self.host, username=self.user, pkey=self.pkey)
@@ -117,7 +118,6 @@ class JobUnit:
             execute_command_with_timeout(self.ssh_client,
                                          self.log, self.err,
                                          'echo FINISHED', timeout = 0.4)
-            
         except Exception as e:
             self.err.error(f'Unexpected Error')
             self.err.error(e)
@@ -201,7 +201,7 @@ def test_jobunit():
     }
     timeout = 0.4
 
-    job_unit = JobUnit(
+    job_unit = JobInstance(
             host, user, pkey, timeout,
             log_stdout, log_stderr,
             cmd_template, arg_config)
@@ -211,6 +211,96 @@ def test_jobunit():
     job_unit.Run()
 
     exit()
+
+def YamlConfiguredJobInstance(yamlLOADEDdict):
+    config = yamlLOADEDdict
+    try:
+        #### configure the status output
+        log_config = config['logging']['stdout']
+        stdout_filter_rules = [ LoggingMgr.errortype_factory(c['filter_method'], c['indicator'],c['threshold'],c['pattern']) for c in log_config['filters'] ]
+        stdout_filter = LoggingMgr.ErrorMessageFilter(stdout_filter_rules)
+        log_stdout = LoggingMgr.configure_logger(log_config['name'],log_config['file'], stdout_filter)
+
+        log_config = config['logging']['stderr']
+        stderr_filter_rules = [ LoggingMgr.errortype_factory(c['filter_method'], c['indicator'],c['threshold'],c['pattern']) for c in log_config['filters'] ]
+        stderr_filter = LoggingMgr.ErrorMessageFilter(stderr_filter_rules)
+        log_stderr = LoggingMgr.configure_logger(log_config['name'],log_config['file'], stderr_filter)
+
+        basic_pars = config['basic_parameters']
+        cmd_templates = config['cmd_templates']
+        cmd_arguments = config['cmd_arguments']
+        job_unit = JobInstance(
+                basic_pars['host'], basic_pars['user'], basic_pars['pkey'], basic_pars['timeout'],
+                log_stdout, log_stderr,
+                cmd_templates, cmd_arguments
+        )
+    except KeyError as e:
+        raise KeyError(f'Invalid key in yaml config "{ config }"') from e
+
+    return job_unit
+def test_YamlConfiguredJobInstance():
+    yaml_content = '''
+basic_parameters:
+  timeout: 0.4
+  host: 'ntugrid8.phys.ntu.edu.tw'
+  user: 'ltsai'
+  pkey: '/Users/noises/.ssh/toNTU8'
+cmd_templates:
+  'init': 'echo "connection established"'
+  'run': 'python3 -u main_job.py; echo "config: {prefix}"'
+  'stop': 'exit'
+cmd_arguments:
+  prefix: default
+logging:
+  stdout:
+    name: out
+    file: log_stdout.txt
+    filters:
+      - indicator: running
+        threshold: 0
+        pattern: 'RUNNING'
+        filter_method: exact
+      - indicator: Type0ERROR
+        threshold: 0
+        pattern: '[running] 0'
+        filter_method: exact
+      - indicator: Type3ERROR
+        threshold: 0
+        pattern: '[running] 3'
+        filter_method: contain
+      - indicator: idle
+        threshold: 0
+        pattern: 'FINISHED'
+        filter_method: exact
+  stderr:
+    name: err
+    file: log_stderr.txt
+    filters:
+      - indicator: running
+        threshold: 0
+        pattern: 'RUNNING'
+        filter_method: exact
+      - indicator: Type0ERROR
+        threshold: 0
+        pattern: '[running] 0'
+        filter_method: exact
+      - indicator: RaiseError
+        threshold: 0
+        pattern: 'Error'
+        filter_method: contain
+        '''
+
+    with open('the_conf.yaml','w') as f:
+        f.write(yaml_content)
+    import yaml
+    with open('the_conf.yaml','r') as f:
+        loaded_conf = yaml.safe_load(f)
+
+    job_instance = YamlConfiguredJobInstance(loaded_conf)
+    job_instance.Initialize()
+    job_instance.Configure( {'prefix': 'confiugred'} )
+    job_instance.Run()
 if __name__ == "__main__":
     #test_direct_run()
-    test_jobunit()
+    #test_jobunit()
+    test_YamlConfiguredJobInstance()
