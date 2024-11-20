8ec4f8936fc3f92c84410e7730d4d5ade83bb052
ltsai323 <johnson20050@gmail.com>
2024-11-20 18:43:03 +0800

    a base class of module

---
+++ ModuleBase.py
@@ -0,0 +1,22 @@
+
+class ModuleBase:
+    '''
+    only define the structure.
+    '''
+    def __init__(self):
+        pass
+
+    def Initialize(self):
+        pass
+
+    def Configure(self, updatedCONF:dict):
+        pass
+
+    def Run(self):
+        pass
+
+    def Stop(self):
+        pass
+
+    def Destroy(self):
+        pass
