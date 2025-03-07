b062748594000758fd92c926f8ded39dac990901
ltsai323 <johnson20050@gmail.com>
2025-03-07 17:14:04 +0800

    update script such as MMTS is able to execute them

--- test/step0.functions.sh
+++ test/step0.functions.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 function exec_at_kria() {
   cmd=$1
-ssh -i /Users/noises/.ssh/hgcalMMTS root@192.168.50.180 $cmd
+#ssh -i /Users/noises/.ssh/hgcalMMTS root@192.168.50.180 $cmd
+ssh -i /home/ntucms/.ssh/HGCtrler_key_kria root@192.168.50.180 $cmd
 }
 function exec_at_ctrlpc() {
   cmd=$1
-ssh -i /Users/noises/.ssh/hgcalMMTS ntucms@192.168.50.144 $cmd
+#ssh -i /Users/noises/.ssh/hgcalMMTS ntucms@192.168.50.144 $cmd
+$cmd
 }
 function the_exit() { echo "ERROR - "$1; exit 1; }
 
+#exec_at_ctrlpc "echo hiii jjj"
--- test/step4.takedata.sh
+++ test/step4.takedata.sh
@@ -4,8 +4,6 @@ source $BASH_SCRIPT_FOLDER/step0.functions.sh
 
 #exec_at_ctrlpc 'sh hii_run.sh'
 exec_at_ctrlpc 'cd /home/ntucms/electronic_test_kria/HD_bottom && python3 pedestal_run.py -i 192.168.50.180 -f initHD-bottom.yaml -d mytest -I && echo FINISHED'
-#ssh ctrlpc 'ls'
-exit
 
 ### errors
 <<LISTED_OUTPUT_MESG
