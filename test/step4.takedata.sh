c21deac6e2bf811758045cd25ab529511b0b6c9e
ntucms <ntucms@localhost.localdomain>
2025-03-08 17:19:21 +0800

    debug

--- test/step4.takedata.sh
+++ test/step4.takedata.sh
@@ -2,8 +2,9 @@
 if [ "$BASH_SCRIPT_FOLDER" == "" ]; then echo "[EnvironmentFailure] Required variable BASH_SCRIPT_FOLDER not set. Load use_python_lib.sh"; exit; fi
 source $BASH_SCRIPT_FOLDER/step0.functions.sh
 
-#exec_at_ctrlpc 'sh hii_run.sh'
+echo 'step 4 running'
 exec_at_ctrlpc 'cd /home/ntucms/electronic_test_kria/HD_bottom && python3 pedestal_run.py -i 192.168.50.180 -f initHD-bottom.yaml -d mytest -I && echo FINISHED'
+echo 'step 4 running ENDED'
 
 ### errors
 <<LISTED_OUTPUT_MESG
