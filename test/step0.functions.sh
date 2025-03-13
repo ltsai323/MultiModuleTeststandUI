36af3b714efc8199d8077a6c66cc1e7afdeeac7c
ltsai323 <johnson20050@gmail.com>
2025-03-13 17:18:58 +0800

    implement data taking that copy the output files to DAQresults/

--- test/step0.functions.sh
+++ test/step0.functions.sh
@@ -11,4 +11,23 @@ eval $cmd
 }
 function the_exit() { echo "ERROR - "$1; exit 1; }
 
+function show_sub_dirs() {
+  # use res=`show_sub_dirs $folder` to get result
+  inFOLDER=$1
+  if [ ! -d "$inFOLDER" ];then return; fi # if folder not found, show nothing
+  abs_in_folder=`realpath $inFOLDER`
+  for a in `ls -d ${abs_in_folder}/*`; do echo $a;done # if folder found, show sub dirs
+}
+##### usage ##########
+# show_sub_dirs $folder > a
+# mkdir ${folder}/aaa
+# show_sub_dirs $folder > b
+# for a  in `comm -13 a b`; do echo hHHHHH $a;done
+# rmdir ${folder}/aaa
+# rm a b
+
+
+
+
+
 #exec_at_ctrlpc "echo hiii jjj"
