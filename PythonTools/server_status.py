8b67dcd7977029887e87db4c61dc87fc3c20bd2a
ltsai323 <johnson20050@gmail.com>
2025-07-04 06:40:59 +0800

    rename var

--- PythonTools/server_status.py
+++ PythonTools/server_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-class ServerStatus:
+class JobModeStatus: # asdf
+    jobmode = ''
+class RunTimeStatus: # asdf
     startup = 'startup'
     initializing = 'initializing' # status when Init() is executing
     initialized = 'initialized' # status of Init() was executed
@@ -16,15 +18,38 @@ class ServerStatus:
 
 
 
-def server_is_runable(currentSTATUS, jobID:str):
-    stat = currentSTATUS
-    if jobID == "Init":
+
+def isCommandRunable(currentSERVERstatus:str, cmdID:str):
+    """
+    Checks whether a command is allowed to run based on the current server status.
+
+    This function maps a command ID to the set of allowed server statuses
+    and returns a boolean indicating whether execution is permitted.
+
+    :param currentSERVERstatus: The current status of the server.
+    :type currentSERVERstatus: str
+    :param cmdID: The command identifier to check. Valid options are:
+                  "Init", "Configure", "Run", "Stop", "Destroy".
+    :type cmdID: str
+    :return: True if the command is runable in the given server status, otherwise False.
+    :rtype: bool
+
+    :Examples:
+
+        >>> isCommandRunable("startup", "Init")
+        True
+        >>> isCommandRunable("idle", "Run")
+        False
+    """
+    stat = currentSERVERstatus
+    if cmdID == "Init":
         return True if stat in [ 'startup', 'destroyed' ] else False
-    if jobID == "Configure":
+    if cmdID == "Configure":
         return True if stat in [ 'initialized', 'stopped', 'idle' ] else False
-    if jobID == "Run":
+    if cmdID == "Run":
         return True if stat in [ 'configured' ] else False
-    if jobID == "Stop":
+    if cmdID == "Stop":
         return True if stat in [ 'running' ] else False
-    if jobID == "Destroy":
+    if cmdID == "Destroy":
         return True if stat in [ 'error', 'initializing', 'initialized', 'configured', 'running', 'idle', 'stopping', 'stopped' ] else False
+
