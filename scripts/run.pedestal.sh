209f1adbdd0bfefdda6912a62f69df51fc8499d2
ltsai323 <johnson20050@gmail.com>
2025-09-17 16:16:54 +0800

    make script more general

--- scripts/run.pedestal.sh
+++ scripts/run.pedestal.sh
@@ -4,10 +4,13 @@ mmtsPOSITION="'$3'"
 moduleSTATUS="'$4'"
 daqINSPECTOR="'$5'"
 
-mmtsCONF=`realpath data/mmts_configurations.yaml`
+mmtsCONF=${FLASK_BASE}/data/mmts_configurations.yaml
 
-cd hgcal-module-testing-gui/
-echo [BASHCMD] python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR || exit 0 ## return 0 prevent make command failed
-python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR || exit 0 ## return 0 prevent make command failed
-#python3 InteractionGUI.directrun.py '../mmts_configurations.yaml' '320-XH-T4C-PM-00019' 'run1' '1L' 'Untaped' 'test-user' || exit 0
+[ "$AndrewModuleTestingGUI_BASE" == "" ] && echo "[InvalidPath] Need to 'source init_bash_bars.sh' before use" && exit 1
+
+cd $AndrewModuleTestingGUI_BASE
+echo [BASHCMD] python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR
+python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR
+[ $? != 0 ] && echo [Error] InteractionGUI.directrun.py got error.  abort... &&  exit 0 ## return 0 prevent make command failed
+#python3 InteractionGUI.directrun.py "'${FLASK_BASE}/data/mmts_configurations.yaml'" '320-XH-T4C-PM-00019' 'run1' '1L' 'Untaped' 'test-user' || exit 0
 cd ..
