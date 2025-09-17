79e50a29b5a45372446fce8a2daf03cfdbc3e499
ltsai323 <johnson20050@gmail.com>
2025-09-17 16:18:12 +0800

    rename file

---
+++ init_bash_vars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+THIS_SCRIPT="${BASH_SOURCE[0]}"
+
+CURRENT_PATH=`dirname $( realpath $THIS_SCRIPT ) `
+export PYTHONPATH=$PWD:$PYTHONPATH
+export LOG_LEVEL=INFO ## DEBUG, INFO, WARNING, CRITICAL
+
+export BASH_SCRIPT_FOLDER=$PWD/scripts/task2_pedestalrun/
+export FLASK_BASE=$CURRENT_PATH
+export AndrewModuleTestingGUI_BASE=/home/ntucms/workspace/MultimoduleTeststandUI/hgcal-module-testing-gui
+
+[ "$AndrewModuleTestingGUI_BASE" == "" ] && echo "[InvalidPath] Need to modify variable 'AndrewModuleTestingGUI_BASE' in  $THIS_SCRIPT" && exit 1;
--- use_python_lib.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=$PWD:$PYTHONPATH
-
-#export BASH_SCRIPT_FOLDER=$PWD/test
-export BASH_SCRIPT_FOLDER=$PWD/scripts/task2_pedestalrun/
