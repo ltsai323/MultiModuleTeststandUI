f83e3914823b96849a66e96fd721181541cecd61
ltsai323 <johnson20050@gmail.com>
2025-09-17 16:54:34 +0800

    check path exist

--- init_bash_vars.sh
+++ init_bash_vars.sh
@@ -3,11 +3,15 @@
 THIS_SCRIPT="${BASH_SOURCE[0]}"
 
 CURRENT_PATH=`dirname $( realpath $THIS_SCRIPT ) `
+
 export PYTHONPATH=$PWD:$PYTHONPATH
 export LOG_LEVEL=INFO ## DEBUG, INFO, WARNING, CRITICAL
 
 export BASH_SCRIPT_FOLDER=$PWD/scripts/task2_pedestalrun/
 export FLASK_BASE=$CURRENT_PATH
-export AndrewModuleTestingGUI_BASE=
 
+
+
+export AndrewModuleTestingGUI_BASE=
 if [ "$AndrewModuleTestingGUI_BASE" == "" ]; then echo "[InvalidPath] Need to modify variable 'AndrewModuleTestingGUI_BASE' in  $THIS_SCRIPT" && exit 1; fi
+if [ ! -d "$AndrewModuleTestingGUI_BASE" ]; then echo "[InvalidPath] variable 'AndrewModuleTestingGUI_BASE' should access correct path"; exit 2; fi
