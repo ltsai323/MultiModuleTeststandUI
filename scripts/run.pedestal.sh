6d7465a0988ef784555e40a3301aeb7a2ec0fb92
ltsai323 <johnson20050@gmail.com>
2025-09-17 13:14:46 +0800

    add script

---
+++ scripts/run.pedestal.sh
@@ -0,0 +1,13 @@
+moduleID="'$1'"
+runTAG="'$2'"
+mmtsPOSITION="'$3'"
+moduleSTATUS="'$4'"
+daqINSPECTOR="'$5'"
+
+mmtsCONF=`realpath data/mmts_configurations.yaml`
+
+cd hgcal-module-testing-gui/
+echo [BASHCMD] python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR || exit 0 ## return 0 prevent make command failed
+python3 InteractionGUI.directrun.py $mmtsCONF $moduleID $runTAG $mmtsPOSITION $moduleSTATUS $daqINSPECTOR || exit 0 ## return 0 prevent make command failed
+#python3 InteractionGUI.directrun.py '../mmts_configurations.yaml' '320-XH-T4C-PM-00019' 'run1' '1L' 'Untaped' 'test-user' || exit 0
+cd ..
