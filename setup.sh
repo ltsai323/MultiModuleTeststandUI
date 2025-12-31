5e3a6afbe196505c6055462710cfe43664621b0b
ltsai323 <johnson20050@gmail.com>
2025-12-31 11:13:45 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 
-if [ ! -d "external_packages/hgcal-module-testing-gui/" ]; then
+if [ -d "external_packages/hgcal-module-testing-gui/" ]; then
+  echo "[ExistedPackage] Andrew's package (DAQ) found";
+else
   echo "[Install Andrew's package (DAQ)] initialize 'hgcal-module-testing-gui' from Andrew "
   echo git clone https://gitlab.cern.ch/acrobert/hgcal-module-testing-gui.git external_packages/hgcal-module-testing-gui
   echo cd external_packages/hgcal-module-testing-gui
@@ -8,7 +10,9 @@ if [ ! -d "external_packages/hgcal-module-testing-gui/" ]; then
 fi
 
 
-if [ ! -d "external_packages/HGCal_Module_Production_Toolkit" ]; then
+if [ -d "external_packages/HGCal_Module_Production_Toolkit" ]; then
+  echo "[ExistedPackage] youying's package (IV scan) found"
+else
   echo "[Install youying's package (IV scan)] initialize 'HGCal_Module_Production_Toolkit' from youying "
   git clone https://github.com/youyingli/HGCal_Module_Production_Toolkit.git external_packages/HGCal_Module_Production_Toolkit
 fi
