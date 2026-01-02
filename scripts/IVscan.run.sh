917fd553bef05e8144a1c5be42c798b60af81a32
ltsai323 <johnson20050@gmail.com>
2026-01-02 20:14:25 +0800

    rename make_iv_curve.py

--- scripts/IVscan.run.sh
+++ scripts/IVscan.run.sh
@@ -9,5 +9,5 @@ scannedIDlist="$6"
 python3 turn_on_HV_switch.py --position="$mmtsPOSITION" --delay="$switchDELAY" --config=../data/mmts_configurations.yaml
 cd ../external_packages/HGCal_Module_Production_Toolkit/
 python3 scripts/getIV.py --module="$moduleID" --temperature="$currentTEMPERATURE" --humidity="$currentHUMIDITY"
-python3 scripts/make_iv_curve2.py "$moduleID" ### make iv curve after scanning finished
-python3 scripts/make_iv_curve2.py --summary `cat "$scannedIDlist"`
+python3 scripts/make_iv_curve.py "$moduleID" ### make iv curve after scanning finished
+python3 scripts/make_iv_curve.py --summary `cat "$scannedIDlist"`
