d9db497ce1a01b228afeff66ed5c3dcb34dea613
ltsai323 <johnson20050@gmail.com>
2026-01-02 09:12:25 +0800

    use my repo instead of youying's repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+if [ ! -f 'data/mmts_configurations.yaml' ]; then 
+  cp data/mmts_configurations.yaml.template data/mmts_configurations.yaml
+  vi data/mmts_configurations.yaml
+fi
 
 if [ -d "external_packages/hgcal-module-testing-gui/" ]; then
   echo "[ExistedPackage] Andrew's package (DAQ) found";
@@ -7,6 +11,7 @@ else
   cd external_packages/hgcal-module-testing-gui
   python3 writeconfig.py
   vi -O configuration.yaml ../hgcal-module-testing-gui-configuration.yaml
+  cd -
 fi
 
 
@@ -14,5 +19,7 @@ if [ -d "external_packages/HGCal_Module_Production_Toolkit" ]; then
   echo "[ExistedPackage] youying's package (IV scan) found"
 else
   echo "[Install youying's package (IV scan)] initialize 'HGCal_Module_Production_Toolkit' from youying "
-  git clone https://github.com/youyingli/HGCal_Module_Production_Toolkit.git external_packages/HGCal_Module_Production_Toolkit
+  git clone https://github.com/ltsai323/HGCal_Module_Production_Toolkit.git external_packages/HGCal_Module_Production_Toolkit
+  ln -s $PWD/data/mmts_configurations.yaml external_packages/HGCal_Module_Production_Toolkit/configurations.yaml
+  mkdir external_packages/HGCal_Module_Production_Toolkit/out
 fi
