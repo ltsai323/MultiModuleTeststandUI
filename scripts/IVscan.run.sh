5258ff4a173672de1743e0925755a261ce4cac11
ltsai323 <johnson20050@gmail.com>
2026-01-02 18:29:02 +0800

    plot iv curve after it finished.

--- scripts/IVscan.run.sh
+++ scripts/IVscan.run.sh
@@ -3,8 +3,9 @@ mmtsPOSITION="$2"
 
 currentTEMPERATURE="$3"
 currentHUMIDITY="$4"
+switchDELAY="$5"
 
-echo python3 turn_on_HV_switch.py --position=$mmtsPOSITION --delay=0 --config=../data/mmts_configurations.yaml
-python3 turn_on_HV_switch.py --position=$mmtsPOSITION --delay=0 --config=../data/mmts_configurations.yaml
+python3 turn_on_HV_switch.py --position="$mmtsPOSITION" --delay="$switchDELAY" --config=../data/mmts_configurations.yaml
 cd ../external_packages/HGCal_Module_Production_Toolkit/
-echo python3 scripts/getIV.py --module=$moduleID --temperature=$currentTEMPERATURE --humidity=$currentHUMIDITY
+python3 scripts/getIV.py --module="$moduleID" --temperature="$currentTEMPERATURE" --humidity="$currentHUMIDITY"
+python3 scripts/make_iv_curve2.py "$moduleID" ### make iv curve after scanning finished
