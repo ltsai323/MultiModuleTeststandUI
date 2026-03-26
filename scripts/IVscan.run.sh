f69747bca002d3bba94e9e4de40d73cb38f2b860
ltsai323 <johnson20050@gmail.com>
2026-03-26 14:30:24 +0800

    1. Use grafana dashboard for generating IV curve.\n2. Disable python script generating IV curve\n3. Disable flask server showing png file.

--- scripts/IVscan.run.sh
+++ scripts/IVscan.run.sh
@@ -1,13 +1,18 @@
 moduleID="$1"
 mmtsPOSITION="$2"
 
-currentTEMPERATURE="$3"
-currentHUMIDITY="$4"
-switchDELAY="$5"
-scannedIDlist="$6"
+maxVOLTAGE="$3"
+currentTEMPERATURE="$4"
+currentHUMIDITY="$5"
+switchDELAY="$6"
+scannedIDlist="$7"
 
 python3 turn_on_HV_switch.py --position="$mmtsPOSITION" --delay="$switchDELAY" --config=../data/mmts_configurations.yaml
 cd ../external_packages/HGCal_Module_Production_Toolkit/
-python3 scripts/getIV.py --module="$moduleID" --temperature="$currentTEMPERATURE" --humidity="$currentHUMIDITY"
-python3 scripts/make_iv_curve.py "$moduleID" ### make iv curve after scanning finished
-python3 scripts/make_iv_curve.py --summary `cat "$scannedIDlist"`
+echo python3 scripts/getIV.py --module="$moduleID" --temperature="$currentTEMPERATURE" --humidity="$currentHUMIDITY" --max_voltage="$maxVOLTAGE"
+python3 scripts/getIV.py --module="$moduleID" --temperature="$currentTEMPERATURE" --humidity="$currentHUMIDITY" --max_voltage="$maxVOLTAGE"
+#python3 scripts/make_iv_curve.py "$moduleID" ### make iv curve after scanning finished
+#python3 scripts/make_iv_curve.py --summary `cat "$scannedIDlist"` ### disable the IV curve plot since I used grafana dashboard generating plot
+cd -
+### reset switch after IV curve scanning
+python3 turn_on_HV_switch.py --position=0 --delay="$switchDELAY" --config=../data/mmts_configurations.yaml
